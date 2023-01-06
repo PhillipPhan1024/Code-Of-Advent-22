1a030bcbd6131cc25fbdaee934b53e5981917241
PhillipPhan1024 <colPhilPhan@gmail.com>
2023-01-05 23:40:28 -0600

    Day 1 Complete

--- day1.java
+++ day1.java
@@ -4,8 +4,12 @@ import java.io.FileNotFoundException;
 public class day1 {
     public static void main(String[] args) throws FileNotFoundException {
         File file = new File("day1Input.txt");
-        Scanner scan = new Scanner(file);
+        partOne(file);
+        partTwo(file);
+    }
 
+    public static void partOne(File file) throws FileNotFoundException {
+        Scanner scan = new Scanner(file);
         // Groups of numbers = one elf, space separates each elf
         int mostCalories = 0;
         int currCalories = 0;
@@ -22,5 +26,30 @@ public class day1 {
         }
         scan.close();
         System.out.println("The Elf carrying the most Calories has: " + mostCalories);
+    }
+
+    public static void partTwo(File file) throws FileNotFoundException {
+        Scanner scan = new Scanner(file);
+        int[] topThree = new int[3];
+        int currCalories = 0;
+        while(scan.hasNextLine()) {
+            String line = scan.nextLine();
+            if(!line.isEmpty()) {
+                currCalories += Integer.parseInt(line);
+            } else {
+                int replacement = currCalories;
+                for(int i = 0; i < topThree.length; i++) {
+                    if(replacement > topThree[i]) {
+                        int temp = topThree[i];
+                        topThree[i] = replacement;
+                        replacement = temp;
+                    }
+                }
+                currCalories = 0;
+            }
+        }
+        scan.close();
+        int mostTotal = topThree[0] + topThree[1] + topThree[2];
+        System.out.println("The three Elves with the most Calories has: " + mostTotal);
     }
 }
